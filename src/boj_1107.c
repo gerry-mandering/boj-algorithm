eabdb6633d8c5a76c287023fd19e0c747110a9c5
gerry-mandering <kimmen1119@gmail.com>
2022-11-24 18:46:33 +0900

    Feat: 백준 1107번 - 리모컨

--- src/boj_1107.c
+++ src/boj_1107.c
@@ -43,24 +43,74 @@ int	is_all_number_available(int channel, int button_bitbask)
 	return (1);
 }
 
-int	get_closest_number(int channel_down, int channel_up, int button_bitbask)
+int	inner_circulate_down(int channel_down, int button_bitbask, int channel, int min_count)
 {
+	int	channel_down_min_count;
+
+	while (1)
+	{
+		channel_down_min_count = get_digit(channel_down) + diff_abs(channel, channel_down);
+		if (channel_down_min_count > min_count)
+			break ;
+		if (is_all_number_available(channel_down, button_bitbask))
+			min_count = channel_down_min_count;
+		if (channel_down == 0)
+			break;
+		channel_down--;
+	}
+	return (min_count);
+}
+
+int	inner_circulate_up(int channel_up, int button_bitbask, int channel, int min_count)
+{
+	int	channel_up_min_count;
+
+	while (1)
+	{
+		channel_up_min_count = get_digit(channel_up) + diff_abs(channel, channel_up);
+		if (channel_up_min_count > min_count)
+			break ;
+		if (is_all_number_available(channel_up, button_bitbask))
+			min_count = channel_up_min_count;
+		if (channel_up == INT_MAX)
+			break;
+		channel_up++;
+	}
+	return (min_count);
+}
+
+int	get_closest_number_min_count(int channel_down, int channel_up, int button_bitbask, int channel)
+{
+	int	min_count;
+
+	if (channel_up != INT_MAX)
+		channel_up++;
+	if (channel_down != 0)
+		channel_down--;
 	while (1)
 	{
 		if (is_all_number_available(channel_up, button_bitbask))
-			return (channel_up);
+		{
+			min_count = get_digit(channel_up) + diff_abs(channel, channel_up);
+			return (inner_circulate_down(channel_down, button_bitbask, channel, min_count));
+		}
 		if (is_all_number_available(channel_down, button_bitbask))
-			return (channel_down);
-		channel_up++;
-		channel_down--;
+		{
+			min_count = get_digit(channel_down) + diff_abs(channel, channel_down);
+			return (inner_circulate_up(channel_up, button_bitbask, channel, min_count));
+		}
+		if (channel_up != INT_MAX)
+			channel_up++;
+		if (channel_down != 0)
+			channel_down--;
 	}
 }
 
 int	main(void)
 {
 	int	button_bitbask = INT_MAX;
-	int	closest_number;
-	int	size, channel, broken_button, min_count;
+	int	closest_number_min_count, plus_minus_button_min_count;
+	int	size, channel, broken_button;
 
 	scanf("%d", &channel);
 	scanf("%d", &size);
@@ -69,20 +119,21 @@ int	main(void)
 		scanf("%d", &broken_button);
 		button_bitbask &= ~(1 << broken_button);
 	}
-	min_count = diff_abs(channel, 100);
+	plus_minus_button_min_count = diff_abs(channel, 100);
 	if (size == 10)
 	{
-		printf("%d\n", min_count);
+		printf("%d\n", plus_minus_button_min_count);
 		return (0);
 	}
-	if (is_all_number_available(channel, button_bitbask) && get_digit(channel) < min_count)
+	if (is_all_number_available(channel, button_bitbask) && get_digit(channel) < plus_minus_button_min_count)
 	{
 		printf("%d\n", get_digit(channel));
 		return (0);
 	}
-	closest_number = get_closest_number(channel - 1, channel + 1, button_bitbask);
-	if (get_digit(closest_number) + diff_abs(channel, closest_number) < min_count)
-		min_count = get_digit(closest_number) + diff_abs(channel, closest_number);
-	printf("%d\n", min_count);
+	closest_number_min_count = get_closest_number_min_count(channel, channel, button_bitbask, channel);
+	if (closest_number_min_count < plus_minus_button_min_count)
+		printf("%d\n", closest_number_min_count);
+	else
+		printf("%d\n", plus_minus_button_min_count);
 	return (0);
 }

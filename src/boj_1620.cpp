0b745d3a65318c924cb5e8ffce7bbe213491e961
gerry-mandering <kimmen1119@gmail.com>
2023-06-07 16:02:18 +0900

    Feat: 1927번 최소 힙 - 우선순위 큐

--- src/boj_1620.cpp
+++ src/boj_1620.cpp
@@ -5,44 +5,47 @@
 #include <utility>
 #include <vector>
 
-int	input_count, output_count;
-std::map<std::string, int>	string_int_map;
-std::vector<std::string>	string_vector;
+int input_count, output_count;
+std::map<std::string, int> string_int_map;
+std::vector<std::string> string_vector;
 
-bool	is_numeric(char input) {
-	if (input >= '0' && input <= '9')
-		return true;
-	else
-		return false;
+bool is_numeric(char input) {
+    if (input >= '0' && input <= '9')
+        return true;
+    else
+        return false;
 }
 
-int	main(void) {
-	std::cin >> input_count >> output_count;
+int main(void) {
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
 
-	std::string	name;
-	for (int i = 0; i < input_count; i++) {
-		std::cin >> name;
-		string_int_map.insert(std::make_pair(name, i + 1));
-		string_vector.push_back(name);
-	}
+    std::cin >> input_count >> output_count;
 
-	std::string								key;
-	std::string::iterator					itor;
-	std::map<std::string, int>::iterator	string_int_itor;
-	std::vector<std::string>::iterator		string_vector_itor;
-	for (int i = 0; i < output_count; i++) {
-		std::cin >> key;
-		itor = key.begin();
-		if (is_numeric(*itor)) {
-			string_vector_itor = string_vector.begin();
-			int	key_num = std::stoi(key);
-			std::advance(string_vector_itor, key_num - 1);
-			std::cout << *string_vector_itor << std::endl;
-		} else {
-			string_int_itor = string_int_map.find(key);
-			std::cout << string_int_itor->second << std::endl;
-		}
-	}
+    std::string name;
+    for (int i = 0; i < input_count; i++) {
+        std::cin >> name;
+        string_int_map.insert(std::make_pair(name, i + 1));
+        string_vector.push_back(name);
+    }
 
-	return 0;
+    std::string key;
+    std::string::iterator itor;
+    std::map<std::string, int>::iterator string_int_itor;
+    std::vector<std::string>::iterator string_vector_itor;
+    for (int i = 0; i < output_count; i++) {
+        std::cin >> key;
+        itor = key.begin();
+        if (is_numeric(*itor)) {
+            string_vector_itor = string_vector.begin();
+            int key_num = std::stoi(key);
+            std::advance(string_vector_itor, key_num - 1);
+            std::cout << *string_vector_itor << std::endl;
+        } else {
+            string_int_itor = string_int_map.find(key);
+            std::cout << string_int_itor->second << std::endl;
+        }
+    }
+
+    return 0;
 }
